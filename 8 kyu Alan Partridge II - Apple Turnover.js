89b7bf23688441f80e55862418ef8295a1d863aa
VitaliyBarahovskiy <68244200+VitaliyBarahovskiy@users.noreply.github.com>
2022-11-05 20:51:45 +0300

    Training Codewars
    JS

---
+++ 8 kyu Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,8 @@
+/* Task
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox. */
+
+function apple(x){
+  if(Number(x)**2 > 1000){
+    return "It's hotter than the sun!!"
+  } else { return "Help yourself to a honeycomb Yorkie for the glovebox."}
+}
